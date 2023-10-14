f25d21e2244d9d6872f0d02b841288a9894a2174
Khangte <115693185+Khangte@users.noreply.github.com>
2023-10-14 23:29:38 +0900

    Update words_list.py

---
+++ words_list.py
@@ -0,0 +1,5 @@
+# 단어 목록
+words= ["alligator", "bear", "car", "dog", "elephant", "frog", "giraffe",
+            "horse", "iguana", "jellyfish", "kangaroo", "lion", "monkey",
+            "narwhal", "owl", "pig", "quail", "rabbit", "snake", "tiger",
+            "unicorn", "vulture", "wolf", "yak", "zebra"]
